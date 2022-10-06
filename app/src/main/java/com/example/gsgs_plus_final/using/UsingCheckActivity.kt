0cc6fe4dcb268ee4388d4f332d33f39b93772d5b
lazygyu <park970406@me.com>
2022-10-06 18:18:43 +0900

    usingcheck activity

---
+++ app/src/main/java/com/example/gsgs_plus_final/using/UsingCheckActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gsgs_plus_final.using
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gsgs_plus_final.R
+
+class UsingCheckActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_using_check)
+    }
+}
